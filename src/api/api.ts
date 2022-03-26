97f9270e3dc9927630f6a3ff3588e0a290c90d24
sinyeongseok <sinyeongseok99@gmail.com>
2022-03-26 23:08:41 +0900

    feat: apiAuth 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 const app: express.Application = express();
 
+import apiAuth from './auth/index';
+app.use('/auth', apiAuth);
+
 export default app;
