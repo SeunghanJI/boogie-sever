45d2a8056895259b8770eafaf6559683dd53d6cb
yuntaengtaeng <k76253218@gmail.com>
2022-03-29 20:49:17 +0900

    feat : Token refreshToken 요청 시 해당 유저가 존재한다면 accessToken, niname, email, isAdmin을 돌려주도록 수정

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,4 +4,7 @@ const app: express.Application = express();
 import apiAuth from './auth/index';
 app.use('/auth', apiAuth);
 
+import apiToken from './token/index';
+app.use('/token', apiToken);
+
 export default app;
--- src/api/token/index.ts
+++ src/api/token/index.ts
@@ -1,25 +1,61 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { Knex } from 'knex';
 const app: express.Application = express();
+const knex: Knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    typeCast: (field: any, next: NextFunction) => {
+      if (field.type === 'TINY' && field.length === 4) {
+        let value = field.string();
+
+        return value ? value === '1' : null;
+      }
+
+      return next();
+    },
+  },
+});
 
 import { verifyRefreshToken, generatedJwtToken } from '../../token/index';
 
-app.post('/refreshToken', verifyRefreshToken, (req: Request, res: Response) => {
-  const email = res.locals.email;
-  const accessToken = generatedJwtToken({
-    email,
-    sub: 'access',
-    expiresIn: '5m',
-  });
+app.post(
+  '/refreshToken',
+  verifyRefreshToken,
+  async (req: Request, res: Response) => {
+    const email = res.locals.email;
+    try {
+      const { id, nickname, is_admin } = await knex('user')
+        .select('id', 'nickname', 'is_admin')
+        .where({ id: email })
+        .first();
 
-  //Todo
-  //email user Table 통해서 존재하는 회원인지 검증 필요
+      if (!id) {
+        Promise.reject();
+      }
 
-  res.json({
-    data: {
-      accessToken,
-      email,
-    },
-  });
-});
+      const accessToken = generatedJwtToken({
+        email,
+        sub: 'access',
+        expiresIn: '5m',
+      });
+
+      res.status(200).json({
+        data: {
+          accessToken,
+          nickname,
+          email: id,
+          isAdmin: is_admin,
+        },
+      });
+    } catch (error) {
+      return res.status(404).json({ message: '가입되지 않은 회원입니다.' });
+    }
+  }
+);
 
 export default app;
