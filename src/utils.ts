cedc2c310f4dd2a710a6ecb46ba865274eae78b1
sinyeongseok <sinyeongseok99@gmail.com>
2022-03-29 20:44:01 +0900

    fix: checkObjectValueEmpty함수 변경
    - 오브젝트 value값으로 false왔을 경우의 수 추가

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { realpathSync } from 'fs';
+
 /*
     해당 Object에 필수로 하는 key값이 존재하는지 검사
     @param {Array} key => array형태로 검사하고 싶은 key 값
@@ -21,11 +23,13 @@ export const checkRequiredProperties = (
     @param {Object} data => 버어있는 vlaue가 존재하는지 검사 할 오브젝트
     @returns boolean => 비어있는 값이 없으면 true 아니면 fasle
 */
-export const checkObjectValueEmpty = (data = {}): boolean => {
+export const checkObjectValueEmpty = (data: {} = {}): boolean => {
   if (!Object.keys(data).length) {
     return false;
   }
 
-  let isSatisfied = Object.values(data).every((value) => !!value);
-  return isSatisfied;
+  let isSatisfied = Object.values(data).every(
+    (value) => value === undefined || value === null || value === ''
+  );
+  return !isSatisfied;
 };
