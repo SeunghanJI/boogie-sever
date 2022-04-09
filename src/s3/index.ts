2b459092224ff52ec4edbd9618ca4057cdfd3f12
SeunghanJI <jsh3581482@naver.com>
2022-04-10 00:19:21 +0900

    fix : multer-s3이용에서 s3.upload함수로 변경

--- src/s3/index.ts
+++ src/s3/index.ts
@@ -1,6 +1,3 @@
-import { Request } from 'express';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
 import AWS from 'aws-sdk';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -21,22 +18,21 @@ const s3 = new AWS.S3({
   region: 'ap-northeast-2',
 });
 
-export const uploadS3WithPath = (
-  path: string,
-  fileName: string | undefined = undefined
-): multer.Multer => {
-  return multer({
-    storage: multerS3({
-      s3,
-      bucket: S3_BUCKET_NAME,
-      contentType: multerS3.AUTO_CONTENT_TYPE,
-      key: (
-        req: Request,
-        file: Express.Multer.File,
-        cb: (error: any, key?: string | undefined) => void
-      ) => {
-        cb(null, `test/${path}/${fileName || file.originalname}`);
-      },
-    }),
+export const uploadFileToS3 = (
+  fileBuffer: Buffer,
+  fileKey: string
+): Promise<AWS.S3.ManagedUpload.SendData> => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      Bucket: S3_BUCKET_NAME,
+      Key: fileKey,
+      Body: fileBuffer,
+    };
+    s3.upload(params, (error: Error, data: AWS.S3.ManagedUpload.SendData) => {
+      if (error) {
+        reject({ code: 500, message: '파일 업로드에 실패하였습니다.' });
+      }
+      resolve(data);
+    });
   });
 };
