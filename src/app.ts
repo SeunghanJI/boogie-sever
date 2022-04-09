018a24a50700814fabc6da9f483f733aa1c506a4
sinyeongseok <sinyeongseok99@gmail.com>
2022-04-09 21:47:02 +0900

    fiet: cookie-parser모듈 추가

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cookieParser from 'cookie-parser';
 import API from './api/api';
 const app: express.Application = express();
 const port: number = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api', API);
 
 app.listen(process.env.PORT || port, () => {
